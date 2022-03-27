62e37351dff1bfd78185824317e3d744e93aa617
bensonirah <bensonirah@gmail.com>
2022-03-27 09:49:00 +0300

    auto open docs in default web browser

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import path from "path";
 import "dotenv/config";
+import open from "open";
 
-const {PORT} = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
 const addExtension = (req, res, next) => {
-  const hasExtension = req.url.endsWith(".html");
-  if (!hasExtension) {
+  if(req.url.endsWith('.json')){
+    next()
+  }
+  const hasHtmlExtension = req.url.endsWith(".html");
+  if (!hasHtmlExtension) {
     const url = req.url.substring(1);
     return res.sendFile(`${url}.html`, { root: process.cwd() });
   }
@@ -21,13 +25,11 @@ app.use("/static", express.static(path.resolve(process.cwd(), "./static")));
 app.use(addExtension);
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: process.cwd() });
-});
-
-app.get("/docs/getting-started", (req, res) => {
   res.sendFile("docs/getting-started.html", { root: process.cwd() });
 });
 
-app.listen(PORT, () => {
-  console.log(`server running on: ${PORT}`);
+app.listen(PORT,async () => {
+  const URL = `http://localhost:${PORT}/docs/getting-started.html`;
+  await open(URL);
+  console.log(`server running on: ${URL}`);
 });
